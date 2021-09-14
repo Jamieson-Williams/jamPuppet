5cc2a7353b20d3be6b3c5a161726e027504737e8
root <root@fore.dev.jams.com.au>
2021-09-14 20:15:09 +1000

    ADDED NODES TO SITE.PP
    This change allows puppet to push out the MOTD module configuration changes to all nodes.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,24 @@
 node default {
-
+  include motd
 }
 
 node 'test.dev.jams.com.au' {
-   include cowsay
+  include cowsay
 }
+
+node 'learn.dev.jams.com.au' {
+  include bashrc
+}
+
+node 'dns.dev.jams.com.au' {
+
+}
+
+node 'dns2.dev.jams.com.au' {
+
+}
+
+node 'ipa.dev.jams.com.au' {
+
+}
+
